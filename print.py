46fd19c91119fce1c60a903518a6cc10413e0285
Akashsoni2653 <87402478+Akashsoni2653@users.noreply.github.com>
2021-07-14 07:34:37 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,11 @@
+a = {1 : 10}
+x = {1 : 100000}
+for y in x :
+    y = y
+    while y <= 100000:
+        print(f"\nTable of {y}")
+        for b in x:
+            while b <= 10 :
+                print(f"{y}x{b} = {y * b}")
+                b += 1
+        y += 1
